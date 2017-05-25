56fdae3c2cfa3bf608a11b0102312e4676823466
Shunki Takami <tkmnet@users.noreply.github.com>
2017-05-25 15:13:19 +0900

    Update: rrsoacis-init.sh

--- rrsoacis-init.sh
+++ rrsoacis-init.sh
@@ -3,3 +3,12 @@
 update-rrsoacis
 
 touch /tmp/ok
+
+cd /home/oacis/rrs-oacis/rrsenv/workspace
+su oacis -c 'mkdir 1'
+cd 1
+su oacis -c "echo \"SERVER_SS='robocup@172.19.0.1'\" > ./config.cfg"
+su oacis -c "echo \"SERVER_C1='robocup@172.19.0.1'\" >> ./config.cfg"
+su oacis -c "echo \"SERVER_C2='robocup@172.19.0.1'\" >> ./config.cfg"
+su oacis -c "echo \"SERVER_C3='robocup@172.19.0.1'\" >> ./config.cfg"
+
